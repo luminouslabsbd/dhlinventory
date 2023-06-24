0a51b3e12fb40dc24367a70b91fe4acea3a983ce
PANNU123 <sajjad.cse35@gmail.com>
2023-06-24 14:13:24 +0600

    jquery remove

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -24,16 +24,16 @@
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>--}}
 {{--<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script> --}}
 
-<script src="{{asset('assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>--}}
 
-<script src="{{asset('assets/plugins/moment/moment.min.js')}}"></script>
-<script src="{{asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/moment/moment.min.js')}}"></script>--}}
+{{--<script src="{{asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>--}}
 
-<script src="{{asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>--}}
 
-<script src="{{asset('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>--}}
 
-<script src="{{asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>--}}
 
 <script src="{{asset('assets/dist/js/adminlte.js')}}"></script>
 
